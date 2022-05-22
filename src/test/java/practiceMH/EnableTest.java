7c5af9dc7770248e296317df02d264150b11f1d8
Vasif Amrahov <mr.vasif.amrahov@gmail.com>
2022-05-22 13:46:37 +0400

    22.05.2022

---
+++ src/test/java/practiceMH/EnableTest.java
@@ -0,0 +1,64 @@
+package practiceMH;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class EnableTest {
+    /*
+    1. Bir class olusturun : EnableTest
+    2. Bir metod olusturun : isEnabled()
+    3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+    4. Textbox’in etkin olmadigini(enabled) dogrulayın
+    5. Enable butonuna tıklayın ve textbox etkin oluncaya kadar bekleyin
+    6. “It’s enabled!” mesajinin goruntulendigini dogrulayın.
+    7. Textbox’in etkin ol dugunu (enabled)
+     */
+
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        //driver.close();
+    }
+
+    @Test
+    public void isEnabledTesti() {
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
+        //4. Textbox’in etkin olmadigini(enabled) dogrulayın
+        WebElement textBoxElement = driver.findElement(By.xpath("//input[@type='text']"));
+        Assert.assertFalse(textBoxElement.isEnabled());
+
+        //5. Enable butonuna tıklayın ve textbox etkin oluncaya kadar bekleyin
+        driver.findElement(By.xpath("//*[text()='Enable']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(textBoxElement));
+        Assert.assertTrue(textBoxElement.isEnabled());
+
+        //6. “It’s enabled!” mesajinin goruntulendigini dogrulayın.
+        WebElement itsEnabledElementi = driver.findElement(By.xpath("//*[text()=\"It's enabled!\"]"));
+        Assert.assertTrue(itsEnabledElementi.isDisplayed());
+
+        //7. Textbox’in etkin oldugunu (enabled) dogrulayin.
+        Assert.assertTrue(textBoxElement.isEnabled());
+    }
+}
---
+++ src/test/java/practiceMH/WaitTest.java
@@ -0,0 +1,86 @@
+package practiceMH;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitTest {
+    /*
+    1. Bir class olusturun : WaitTest
+    2. Iki tane metod olusturun : implicitWaitTesti() , explicitWaitTesti()
+       Iki metod icin de asagidaki adimlari test edin.
+    3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+    4. Remove butonuna basin.
+    5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+    6. Add buttonuna basin
+    7. It’s back mesajinin gorundugunu test edin
+     */
+
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void implicitWaitTesti() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
+        //4. Remove butonuna basin.
+        driver.findElement(By.xpath("//*[text()='Remove']")).click();
+
+        //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+        WebElement itsGoneElementi = driver.findElement(By.xpath("//*[text()=\"It's gone!\"]"));
+        Assert.assertTrue(itsGoneElementi.isDisplayed());
+
+        //6. Add buttonuna basin
+        driver.findElement(By.xpath("//*[text()='Add']")).click();
+
+        //7. It’s back mesajinin gorundugunu test edin
+        WebElement itsBackElementi = driver.findElement(By.xpath("//*[text()=\"It's back!\"]"));
+        Assert.assertTrue(itsBackElementi.isDisplayed());
+    }
+
+    @Test
+    public void explicitWaitTesti() {
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
+        //4. Remove butonuna basin.
+        driver.findElement(By.xpath("//*[text()='Remove']")).click();
+
+        //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement itsGoneElementi = wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.xpath("//*[text()=\"It's gone!\"]")));
+        Assert.assertTrue(itsGoneElementi.isDisplayed());
+
+        //6. Add buttonuna basin
+        driver.findElement(By.xpath("//*[text()='Add']")).click();
+
+        //7. It’s back mesajinin gorundugunu test edin
+        WebElement itsBackElementi = wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.xpath("//*[text()=\"It's back!\"]")));
+        Assert.assertTrue(itsBackElementi.isDisplayed());
+    }
+}
