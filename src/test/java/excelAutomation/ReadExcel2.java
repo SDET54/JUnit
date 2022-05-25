b513a9028edecbe313c0de1b94dbaa016311dc8a
Vasif Amrahov <mr.vasif.amrahov@gmail.com>
2022-05-25 15:38:01 +0400

    25.05.2022

--- src/test/java/excelAutomation/ReadExcel2.java
+++ src/test/java/excelAutomation/ReadExcel2.java
@@ -28,31 +28,31 @@ public class ReadExcel2 {
         //- 1.satirdaki 2.hucreye gidelim ve yazdiralim
         int satirNo = 1;
         int sutunNo = 2;
-        System.out.println(hucreBilgisiGetir(satirNo-1, sutunNo-1));
+        System.out.println(hucreBilgisiGetir(satirNo - 1, sutunNo - 1));
 
         //- 1.satirdaki 2.hucreyi bir string degiskene atayalim ve yazdiralim
-        String hucreBilgisi = hucreBilgisiGetir(satirNo-1, sutunNo-1);
+        String hucreBilgisi = hucreBilgisiGetir(satirNo - 1, sutunNo - 1);
         System.out.println("hucreBilgisi = " + hucreBilgisi);
 
         //-2.satir 4.cell’in afganistan’in baskenti oldugunu test edelim
         satirNo = 2;
-        sutunNo=4;
-        hucreBilgisi = hucreBilgisiGetir(satirNo-1, sutunNo-1);
+        sutunNo = 4;
+        hucreBilgisi = hucreBilgisiGetir(satirNo - 1, sutunNo - 1);
 
-        Assert.assertEquals("Kabil",hucreBilgisi);
+        Assert.assertEquals("Kabil", hucreBilgisi);
 
         //-Satir sayisini bulalim
         FileInputStream fis = new FileInputStream("src/resources/ulkeler.xlsx");
         Workbook workbook = WorkbookFactory.create(fis);
         int satirSayisiIndex = workbook.getSheet("Sayfa1").getLastRowNum();
-        System.out.println("satirSayisi = " + (satirSayisiIndex+1));
+        System.out.println("satirSayisi = " + (satirSayisiIndex + 1));
 
         //-Fiziki olarak kullanilan satir sayisini bulun
         int kullanilanSatirSayisi = workbook.getSheet("Sayfa1").getPhysicalNumberOfRows();
         System.out.println("kullanilanSatirSayisi = " + kullanilanSatirSayisi);
 
         //-Ingilizce Ulke isimleri ve baskentleri bir map olarak kaydedelim
-        Map<String,String> ulkelerMap = new HashMap<>();
+        Map<String, String> ulkelerMap = new HashMap<>();
 
         for (int i = 0; i <= satirSayisiIndex; i++) {
             String keys = workbook.getSheet("Sayfa1").getRow(i).getCell(0).toString();
@@ -61,7 +61,7 @@ public class ReadExcel2 {
                     ", " + workbook.getSheet("Sayfa1").getRow(i).getCell(2).toString() +
                     ", " + workbook.getSheet("Sayfa1").getRow(i).getCell(3).toString();
 
-            ulkelerMap.put(keys,value);
+            ulkelerMap.put(keys, value);
 
         }
 
@@ -70,7 +70,7 @@ public class ReadExcel2 {
     }
 
     public String hucreBilgisiGetir(int satir, int sutun) throws IOException {
-        String hucreBilgisi="";
+        String hucreBilgisi = "";
         FileInputStream fis = new FileInputStream("src/resources/ulkeler.xlsx");
         Workbook workbook = WorkbookFactory.create(fis);
         hucreBilgisi = workbook.
